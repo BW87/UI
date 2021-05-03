b67c2818db281b5673acfcf8ffe30484da6067ac
SeoBoWon <qhdnjs87@gmail.com>
2021-05-04 01:20:32 +0900

    PersonalInformationActivity suffix focus 될때만 보이는거 고쳐야함

--- app/src/main/java/com/example/test/PersonalInformationActivity.kt
+++ app/src/main/java/com/example/test/PersonalInformationActivity.kt
@@ -10,13 +10,17 @@ import kotlinx.android.synthetic.main.activity_personal_information.*
 class PersonalInformationActivity : AppCompatActivity() {
 
     val genderItems = listOf("Male", "Female", "Etc")
-    val genderAdapter = ArrayAdapter(this, R.layout.gender_item, genderItems)
+    val bloodItems = listOf("A", "B", "O", "AB")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_information)
 
+        val genderAdapter = ArrayAdapter(this, R.layout.gender_item, genderItems)
         (personal_gender_edit.editText as AutoCompleteTextView).setAdapter(genderAdapter)
+
+        val bloodAdapter = ArrayAdapter(this, R.layout.blood_type_item, bloodItems)
+        (blood_type_edit.editText as AutoCompleteTextView).setAdapter(bloodAdapter)
     }
 
     fun personalBack(view: View) {}
